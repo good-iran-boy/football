79766548ff502ce07778aa7609c3932f2e6d2684
reza <rezza.op@gmail.com>
2021-06-18 21:27:17 +0430

    game component and select added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
 import React, { useEffect, useState } from 'react';
 import Layout from "./components/Layout/Layout";
-import Parallelogram from "./components/Parallelogram";
+import Game from './components/Game';
+
 function App() {
   const [state, setstate] = useState([])
+  const [url, seturl] = useState('https://api.football-data.org/v2/competitions/2021/matches?season=2020&dateFrom=2020-09-12&dateTo=2020-09-18')
   useEffect(() => {
 
-    fetch('https://api.football-data.org/v2/competitions/2021/matches?season=2020', {
+    fetch('http://api.football-data.org/v2/teams/100', {
       method: "GET", headers: {
         "X-Auth-Token": "bfcd78c4f4da41098dc729f7f6570d26",
         accept: 'application/json'
@@ -18,9 +19,10 @@ function App() {
   console.log(state);
   return (
     <Layout >
-<Parallelogram>بازی های لیگ برتر انگلیس</Parallelogram>
+      <Game/>
     </Layout>
   );
 }
 
 export default App;
+// 'http://api.football-data.org/v2/teams/57'
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import styles from "../../styles/Layout.module.css";
+import Parallelogram from "../Parallelogram";
+import Select from "../Select";
 export default function Layout({ children }) {
     return (
         <div className={styles.box}>
             <div className={styles.col}>
-            {children}
+                <div className={styles.title}>
+                    <Select />
+                    <Parallelogram>بازی های لیگ برتر انگلیس</Parallelogram>
+                </div>
+
+                {children}
             </div>
         </div>
     )
